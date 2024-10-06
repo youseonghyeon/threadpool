5f466f82d1f55eac085ddcbfb18d73bfc0766ac1
seonghyeon <dolla_@naver.com>
2024-10-06 21:14:47 +0900

    주석 추가

--- src/thread/MyThreadPoolService.java
+++ src/thread/MyThreadPoolService.java
@@ -9,15 +9,27 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyThreadPoolService {
+    // minThreads와 maxThreads는 최소 스레드 개수와 최대 스레드 개수를 의미합니다.
     private final int minThreads;
     private final int maxThreads;
+    // taskQueue는 작업을 담는 큐입니다.
     private final BlockingQueue<Runnable> taskQueue;
+    // activeThreadList는 현재 활성화된 스레드를 관리하는 리스트입니다.
     private final List<WorkerThread> activeThreadList;
+    // isStopped는 스레드 풀이 종료되었는지 여부를 나타내는 플래그입니다.
     private final AtomicBoolean isStopped = new AtomicBoolean(false);
 
+    // 스레드의 타임아웃 시간을 나타내는 상수입니다.
     private final int threadTimeout = 10000;
 
+    /**
+     * MyThreadPoolService 생성자입니다.
+     * 생성자 실행 시 WatchDog 쓰레드가 시작됩니다.
+     */
     public MyThreadPoolService(int minThreads, int maxThreads) {
+        if (minThreads < 1 || maxThreads < 1 || minThreads > maxThreads) {
+            throw new IllegalArgumentException("Invalid thread pool size");
+        }
         this.minThreads = minThreads;
         this.maxThreads = maxThreads;
         this.taskQueue = new LinkedBlockingQueue<>();
@@ -25,6 +37,10 @@ public class MyThreadPoolService {
         startWatchDogThread();
     }
 
+    /**
+     * 스레드 풀에 작업을 추가합니다.
+     * 스레드 풀이 종료된 경우 IllegalStateException을 발생시킵니다.
+     */
     public void execute(Runnable task) {
         if (isStopped.get()) {
             throw new IllegalStateException("Thread pool is stopped");
@@ -35,6 +51,9 @@ public class MyThreadPoolService {
         taskQueue.offer(task);
     }
 
+    /**
+     * 스레드 풀을 종료합니다.
+     */
     public void shutdown() {
         isStopped.set(true);
         for (WorkerThread worker : activeThreadList) {
@@ -42,12 +61,18 @@ public class MyThreadPoolService {
         }
     }
 
+    /**
+     * 새로운 쓰레드를 생성하고 activeThreadList에 추가합니다.
+     */
     private void generateWorkerThread() {
         WorkerThread worker = new WorkerThread(taskQueue);
         activeThreadList.add(worker);
         worker.start();
     }
 
+    /**
+     * WatchDog 쓰레드를 시작합니다.
+     */
     private void startWatchDogThread() {
         new Thread(() -> {
             while (!isStopped.get()) {
@@ -59,10 +84,16 @@ public class MyThreadPoolService {
         }).start();
     }
 
+    /**
+     * 의도하지 않게 종료된 스레드를 activeThreadList 에서 제거합니다.
+     */
     private void removeDeadThreads() {
         activeThreadList.removeIf(workerThread -> !workerThread.isAlive());
     }
 
+    /**
+     * 타임아웃된 스레드를 한개씩 activeThreadList에서 제거합니다.
+     */
     private void removeTimedOutThread() {
         if (activeThreadList.size() > minThreads) {
             Optional<WorkerThread> targetThread = activeThreadList.stream()
@@ -75,6 +106,21 @@ public class MyThreadPoolService {
         }
     }
 
+    /**
+     * 스레드가 타임아웃되었는지 확인합니다.
+     */
+    private boolean timeOver(WorkerThread workerThread) {
+        long lastStartTime = workerThread.getLastStartTime();
+        return System.currentTimeMillis() - lastStartTime > threadTimeout;
+    }
+
+
+    /**
+     * 최소 스레드 개수를 유지합니다.
+     * <p>
+     *     taskQueue에 작업이 있으면서, 쓰레드가 모두 종료된 경우 작업을 실행하지 못하는데 이를 방지합니다.
+     * </p>
+     */
     private void maintainMinThreads() {
         if (activeThreadList.size() < minThreads) {
             int currentThreadSize = activeThreadList.size();
@@ -82,12 +128,6 @@ public class MyThreadPoolService {
                 generateWorkerThread();
             }
         }
-    }
-
-
-    private boolean timeOver(WorkerThread workerThread) {
-        long lastStartTime = workerThread.getLastStartTime();
-        return System.currentTimeMillis() - lastStartTime > threadTimeout;
     }
 
     private static void sleep(long millis) {
--- src/thread/WorkerThread.java
+++ src/thread/WorkerThread.java
@@ -3,11 +3,14 @@ package thread;
 import java.util.concurrent.BlockingQueue;
 
 class WorkerThread extends Thread {
+    // taskQueue는 작업을 담는 큐입니다. 부모 클래스인 MyThreadPoolService에서 초기화하며 공유합니다.
     private final BlockingQueue<Runnable> taskQueue;
+    // isStopped는 스레드가 종료되었는지 여부를 나타내는 플래그입니다.
     private boolean isStopped = false;
+    // lastStartTime은 마지막으로 작업을 수행한 시간을 나타냅니다.
     private long lastStartTime;
 
-    public WorkerThread(BlockingQueue<Runnable> taskQueue) {
+    protected WorkerThread(BlockingQueue<Runnable> taskQueue) {
         this.taskQueue = taskQueue;
     }
 
@@ -16,6 +19,7 @@ class WorkerThread extends Thread {
         while (!isStopped) {
             try {
                 lastStartTime = System.currentTimeMillis();
+                // 블로킹 큐에서 작업을 실행하므로 따로 time sleep을 사용하지 않습니다.
                 taskQueue.take().run();
             } catch (InterruptedException e) {
                 System.out.println("[WARN] Thread interrupted " + Thread.currentThread().getName());
@@ -25,11 +29,11 @@ class WorkerThread extends Thread {
         }
     }
 
-    public long getLastStartTime() {
+    protected long getLastStartTime() {
         return lastStartTime;
     }
 
-    public void stopWorker() {
+    protected void stopWorker() {
         isStopped = true;
         this.interrupt(); // 인터럽트를 통해 종료
     }
